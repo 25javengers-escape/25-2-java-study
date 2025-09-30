523c2ff2c883c924033ba13ac4d598886c42ab96
chaeyeon613 <ichaeyeon090@gmail.com>
2025-09-30 17:25:43 +0900

    [refactor] WrapperTest2 for문을 이용하여 최적화

--- java-mid1-chaeyeon/src/lang/wrapper/test/WrapperTest2.java
+++ java-mid1-chaeyeon/src/lang/wrapper/test/WrapperTest2.java
@@ -5,12 +5,11 @@ public class WrapperTest2 {
         String[] array = {"1.5", "2.5", "3.0"};
 
         //코드 작성
-        double arr1 = Double.parseDouble(array[0]);
-        double arr2 = Double.parseDouble(array[1]);
-        double arr3 = Double.parseDouble(array[2]);
-
-        double sum = arr1 + arr2 + arr3;
-
+        double sum = 0;
+        for (String s : array) {
+            double i = Double.parseDouble(s);
+            sum += i;
+        }
         System.out.println("sum = " + sum);
     }
 }

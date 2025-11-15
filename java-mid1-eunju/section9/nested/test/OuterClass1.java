f2e66eb9d6ebb016865e0e5b5332e07ce51ee1c7
“ej3131” <jangeunju@sookmyung.ac.kr>
2025-11-15 21:45:13 +0900

    Upload section9 문제풀이

---
+++ java-mid1-eunju/section9/nested/test/OuterClass1.java
@@ -0,0 +1,9 @@
+package section9.nested.test;
+
+public class OuterClass1 {
+    static class NestedClass {
+        public void hello(){
+            System.out.println("NestedClass.hello");
+        }
+    }
+}

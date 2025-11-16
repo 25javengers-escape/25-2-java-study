e6243f525065fb286b048be76f55014bc9ddc5ae
seoeun.P <seoeun.p@sookmyung.ac.kr>
2025-11-16 15:21:10 +0900

    structure change

---
+++ java-mid1-seoeun/src/Section4/TestString7.java
@@ -0,0 +1,10 @@
+package Section4;
+
+public class TestString7 {
+    public static void main(String[] args) {
+        String original = " Hello Java ";
+
+        String modified = original.trim();
+        System.out.println(modified);
+    }
+}
